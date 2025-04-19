0ee8180e7687963c341a5391f27e6ed4da63c2d5
qiandrewj <qiandrewj@gmail.com>
2025-04-19 14:20:56 -0400

    block tests

--- test/test_blockblast.ml
+++ test/test_blockblast.ml
@@ -1,6 +1,10 @@
 open OUnit2
+open Blockblast
 
-let tests =
-  "test suite" >::: [ ("a trivial test" >:: fun _ -> assert_equal 0 0) ]
+let test_block _ =
+  let b = Block.create_block Block.R Block.sqr in
+  assert_equal Block.R (Block.get_color b);
+  assert_equal Block.sqr (Block.get_shape b)
 
+let tests = "test suite" >::: [ "block tests" >:: test_block ]
 let _ = run_test_tt_main tests
